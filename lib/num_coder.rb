4d5636d9b59885247504a897a7bb74719400ba2d
Danilo Moret <github@moret.pro.br>
2012-09-01 20:02:29 -0300

    added new fixed size encoding and decoding

--- lib/num_coder.rb
+++ lib/num_coder.rb
@@ -1,35 +1,66 @@
 class NumCoder
   class << self
 
-    def encode(cs, i)
-      s = ""
-      t = i
-      while t > 0
-        s = cs[t % cs.size] + s
-        t = t / cs.size
+    def fixed_encode(cs, i, l)
+      if i.is_a? Array
+        r = ""
+        i.each { |e| r << fixed_encode(cs, e, l) }
+        return r
+      else
+        raise "Won't fit" unless i < cs.size ** l
+        s = cs[0] * l
+        l.times do |p|
+          s[-1-p] = cs[i / cs.size ** p % cs.size]
+        end
+        return s
       end
-      return s
+    end
+
+    def fixed_decode(cs, s, l)
+      raise "Can't split" unless s.size % l == 0
+      r = []
+      (s.size / l).times do |p|
+        i = 0
+        s[p*l..p*l+l-1].each_char { |c| i = cs.size * i + cs.index(c) }
+        r << i
+      end
+
+      if r.size == 1
+        return r[0]
+      else
+        return r
+      end
+    end
+
+    def encode(cs, i)
+      fixed_encode(cs, i, Math.log(i, cs.size).ceil)
     end
 
     def decode(cs, s)
-      i = 0
-      s.each_char { |c| i = cs.size * i + cs.index(c) }
-      return i
+      fixed_decode(cs, s, s.size)
     end
 
     def cs95
-      cs = ""
-      32.upto(126) { |c| cs << c }
-      return cs
-      # " !\"#\$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+      # cs = ""
+      # 32.upto(126) { |c| cs << c }
+      # return cs
+      " !\"#\$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
     end
 
     def encode95(i)
       encode(cs95, i)
     end
 
+    def fixed_encode95(i, l)
+      fixed_encode(cs95, i, l)
+    end
+
     def decode95(s)
       decode(cs95, s)
+    end
+
+    def fixed_decode95(s, l)
+      fixed_decode(cs95, s, l)
     end
 
     def cs64
@@ -40,8 +71,16 @@ class NumCoder
       encode(cs64, i)
     end
 
+    def fixed_encode64(i, l)
+      fixed_encode(cs64, i, l)
+    end
+
     def decode64(s)
       decode(cs64, s)
+    end
+
+    def fixed_decode64(s, l)
+      fixed_decode(cs64, s, l)
     end
 
     def cs16
@@ -52,8 +91,16 @@ class NumCoder
       encode(cs16, i)
     end
 
+    def fixed_encode16(i, l)
+      fixed_encode(cs16, i, l)
+    end
+
     def decode16(s)
       decode(cs16, s)
+    end
+
+    def fixed_decode16(s, l)
+      fixed_decode(cs16, s, l)
     end
 
   end
--- spec/lib/num_coder_spec.rb
+++ spec/lib/num_coder_spec.rb
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 require './spec/spec_helper'
 require './lib/num_coder'
 
 describe NumCoder do
-  it "should be better tested" do
+
+  it "should encode and decode on the default bases" do
     NumCoder.encode95(12345678901234567890).should eq("3Wvh@TZMqf")
     NumCoder.decode95("Hadoken!").should eq(2841662827835761)
     NumCoder.encode64(12345678901234567890).should eq("KtUqYzrHwrS")
@@ -10,4 +12,46 @@ describe NumCoder do
     NumCoder.encode16(12345678901234567890).should eq("ab54a98ceb1f0ad2")
     NumCoder.decode16("b00b5").should eq(721077)
   end
+
+  it "should reject characters outside the default bases" do
+    expect { NumCoder.decode95("Hádoken!") }.to raise_error
+    expect { NumCoder.decode64("Hádoken!") }.to raise_error
+    expect { NumCoder.decode64("Hádoken!") }.to raise_error
+  end
+
+  it "should encode to fixed size on the default bases" do
+    NumCoder.fixed_encode95(1234567890, 5).size.should eq(5)
+    NumCoder.fixed_encode95(12, 5).size.should eq(5)
+    NumCoder.fixed_encode64(1234567890, 6).size.should eq(6)
+    NumCoder.fixed_encode64(12, 6).size.should eq(6)
+    NumCoder.fixed_encode16(1234567890, 8).size.should eq(8)
+    NumCoder.fixed_encode16(12, 8).size.should eq(8)
+  end
+
+  it "should encode arrays to fixed size on base 95" do
+    numbers = [1234567890, 2345678901, 3456789012]
+    encoded_95 = NumCoder.fixed_encode95(numbers, 5)
+    encoded_95.should eq("/.y5M<kt!qJInch")
+  end
+
+  it "should reject to fix encode numbers too large to fit" do
+    expect { NumCoder.fixed_encode95(1234567890, 4) }.to raise_error
+    expect { NumCoder.fixed_encode64(1234567890, 5) }.to raise_error
+    expect { NumCoder.fixed_encode16(1234567890, 7) }.to raise_error
+  end
+
+  it "should decode strings as arrays of numbers on fix decode 95" do
+    decoded_95 = NumCoder.fixed_decode95("/.y5M<kt!qJInch", 5)
+    decoded_95.size.should eq(3)
+    decoded_95[0].should eq(1234567890)
+    decoded_95[1].should eq(2345678901)
+    decoded_95[2].should eq(3456789012)
+  end
+
+  it "should reject to fix decode strings not multiple of limit" do
+    expect { NumCoder.fixed_decode95("abcdef", 5) }.to raise_error
+    expect { NumCoder.fixed_decode64("abcdef", 5) }.to raise_error
+    expect { NumCoder.fixed_decode16("abcdef", 5) }.to raise_error
+  end
+
 end
